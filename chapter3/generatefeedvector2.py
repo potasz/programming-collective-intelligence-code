b8050b957311c6f07896a58163c838173f4e4f57
Szabolcs Pota <spota@thunderhead.com>
2015-08-27 16:49:18 +0200

    Minor modification for NOS feeds.

--- chapter3/generatefeedvector2.py
+++ chapter3/generatefeedvector2.py
@@ -2,6 +2,7 @@ import feedparser
 import re
 
 ARS_RSS_FEED_URL='http://feeds.arstechnica.com/arstechnica/index'
+NOS_RSS_FEED_URL='http://www.nos.nl/export/nosnieuws-rss.xml'
 
 # Returns title and dictionary of word counts for an RSS feed
 def getwordcounts(url):
@@ -12,12 +13,23 @@ def getwordcounts(url):
   # Loop over all the entries
   for e in d.entries:
     # Extract a list of words
-    words=getwords(e.title + ' ' + e.content[0]['value'])
+    if 'content' in e and len(e.content) > 1 and 'value' in e.content[0]:
+      content = e.content[0]['value']
+    else:
+      content = e.summary
+
+    words=getwords(e.title + ' ' + content)
     wc = {}
     for word in words:
       wc.setdefault(word,0)
       wc[word]+=1
-    titles[e.feedburner_origlink] = wc
+
+    if 'feedburner_origlink' in e:
+      key = e.feedburner_origlink
+    else:
+      key = e.title
+
+    titles[key] = wc
   return titles
 
 def getwords(html):
@@ -32,7 +44,7 @@ def getwords(html):
 
 
 apcount={}
-wordcounts = getwordcounts(ARS_RSS_FEED_URL)
+wordcounts = getwordcounts(NOS_RSS_FEED_URL)
 #print wordcounts
 for title,wc in wordcounts.items():
   for word,count in wc.items():
@@ -45,7 +57,7 @@ for w,bc in apcount.items():
   if frac>0 and frac<0.1:
     wordlist.append(w)
 
-out=file('ars_data.txt','w')
+out=file('nos_data.txt','w')
 out.write('Title')
 for word in wordlist: out.write('\t%s' % word)
 out.write('\n')
